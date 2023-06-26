df3387fd6437386d8d24f20ec0c2a8d705b5b294
Tomomichi Sugihara <zhidao@ieee.org>
2023-06-27 00:44:53 +0900

    Modified rkglTextureBumpReadFile and _rkglTextureBumpLightMap to use rkgl_cubemap_id. Abolished _rkglTextureBumpNormalX and _rkglTextureBumpNormalY, and applied zxImageNormalVec in ZX11.

--- src/rkgl_texture.c
+++ src/rkgl_texture.c
@@ -191,54 +191,6 @@ void rkglTextureDisableProjection(void)
 
 /* bump mapping */
 
-/* x component of a normal vector on a bump texture */
-static double _rkglTextureBumpNormalX(zxImage *img, zxPixelManip *pm, uint j, uint i)
-{
-  ubyte r1, g1, b1;
-  ubyte r2, g2, b2;
-  ubyte r3, g3, b3;
-
-  if( j <= 0 ){
-    zxImageCellRGB( img, pm, 0, i, &r1, &g1, &b1 );
-    zxImageCellRGB( img, pm, 1, i, &r2, &g2, &b2 );
-    zxImageCellRGB( img, pm, 2, i, &r3, &g3, &b3 );
-    return -0.5 * (double)( r1 + g1 + b1 ) + 2 * (double)( r2 + g2 + b2 ) / 3 - (double)( r3 + g3 + b3 ) / 6;
-  }
-  if( j + 1 >= img->width ){
-    zxImageCellRGB( img, pm, img->width-3, i, &r1, &g1, &b1 );
-    zxImageCellRGB( img, pm, img->width-2, i, &r2, &g2, &b2 );
-    zxImageCellRGB( img, pm, img->width-1, i, &r3, &g3, &b3 );
-    return (double)( r1 + g1 + b1 ) / 6 - 2 * (double)( r2 + g2 + b2 ) / 3 + 0.5 * (double)( r3 + g3 + b3 );
-  }
-  zxImageCellRGB( img, pm, j-1, i, &r1, &g1, &b1 );
-  zxImageCellRGB( img, pm, j+1, i, &r3, &g3, &b3 );
-  return (double)( r3 + g3 + b3 - r1 - g1 - b1 ) / 6;
-}
-
-/* y component of a normal vector on a bump texture */
-static double _rkglTextureBumpNormalY(zxImage *img, zxPixelManip *pm, uint j, uint i)
-{
-  ubyte r1, g1, b1;
-  ubyte r2, g2, b2;
-  ubyte r3, g3, b3;
-
-  if( i <= 0 ){
-    zxImageCellRGB( img, pm, j, 0, &r1, &g1, &b1 );
-    zxImageCellRGB( img, pm, j, 1, &r2, &g2, &b2 );
-    zxImageCellRGB( img, pm, j, 2, &r3, &g3, &b3 );
-    return -0.5 * (double)( r1 + g1 + b1 ) + 2 * (double)( r2 + g2 + b2 ) / 3 - (double)( r3 + g3 + b3 ) / 6;
-  }
-  if( i + 1 >= img->height ){
-    zxImageCellRGB( img, pm, j, img->height-3, &r1, &g1, &b1 );
-    zxImageCellRGB( img, pm, j, img->height-2, &r2, &g2, &b2 );
-    zxImageCellRGB( img, pm, j, img->height-1, &r3, &g3, &b3 );
-    return (double)( r1 + g1 + b1 ) / 6 - 2 * (double)( r2 + g2 + b2 ) / 3 + 0.5 * (double)( r3 + g3 + b3 );
-  }
-  zxImageCellRGB( img, pm, j, i-1, &r1, &g1, &b1 );
-  zxImageCellRGB( img, pm, j, i+1, &r3, &g3, &b3 );
-  return (double)( r3 + g3 + b3 - r1 - g1 - b1 ) / 6;
-}
-
 /* convert a direction vector to a texture value */
 static ubyte *_rkglTextureBumpVec(ubyte *p, double x, double y, double z)
 {
@@ -255,7 +207,7 @@ static bool _rkglTextureBumpNormalMap(zTexture *bump, char *filename)
   uint i, j, k;
   zxImage img;
   zxPixelManip pm;
-  double nx, ny, nz, l;
+  double nx, ny, nz;
   bool already_connected, ret = true;
 
   already_connected = !zxInit();
@@ -270,13 +222,10 @@ static bool _rkglTextureBumpNormalMap(zTexture *bump, char *filename)
     ZRUNERROR( "zero-depth bump unrenderable" );
     bump->depth = 1.0;
   }
-  nz = 1.0 / bump->depth;
   for( k=0, i=0; i<img.height; i++ ){
     for( j=0; j<img.width; j++, k+=4 ){
-      nx = _rkglTextureBumpNormalX( &img, &pm, j, i );
-      ny = _rkglTextureBumpNormalY( &img, &pm, j, i );
-      l = sqrt( nx*nx + ny*ny + nz*nz );
-      _rkglTextureBumpVec( bump->buf + k, nx/l, ny/l, nz/l );
+      zxImageNormalVec( &img, &pm, j, i, &nx, &ny, &nz );
+      _rkglTextureBumpVec( bump->buf + k, nx, ny, nz );
     }
   }
  TERMINATE:
@@ -303,12 +252,12 @@ static void _rkglTextureBumpLightMap(zTexture *bump)
       zr = 1.0 / sqrt( x*x + y2 );
       xr = x * zr;
       yr = y * zr;
-      _rkglTextureBumpVec( bump->lbuf[0] + k, -zr, -yr,  xr );
-      _rkglTextureBumpVec( bump->lbuf[1] + k,  xr, -zr, -yr );
-      _rkglTextureBumpVec( bump->lbuf[2] + k, -xr, -yr, -zr );
-      _rkglTextureBumpVec( bump->lbuf[3] + k,  zr, -yr, -xr );
-      _rkglTextureBumpVec( bump->lbuf[4] + k,  xr,  zr,  yr );
-      _rkglTextureBumpVec( bump->lbuf[5] + k,  xr, -yr,  zr );
+      _rkglTextureBumpVec( bump->lbuf[0] + k,  zr, -yr, -xr );
+      _rkglTextureBumpVec( bump->lbuf[1] + k, -zr, -yr,  xr );
+      _rkglTextureBumpVec( bump->lbuf[2] + k,  xr,  zr,  yr );
+      _rkglTextureBumpVec( bump->lbuf[3] + k,  xr, -zr, -yr );
+      _rkglTextureBumpVec( bump->lbuf[4] + k,  xr, -yr,  zr );
+      _rkglTextureBumpVec( bump->lbuf[5] + k, -xr, -yr, -zr );
     }
   }
 }
@@ -316,14 +265,6 @@ static void _rkglTextureBumpLightMap(zTexture *bump)
 /* create a bump map */
 bool rkglTextureBumpReadFile(zTexture *bump, char *filename)
 {
-  static int cmap_type[] = {
-    GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
-    GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
-    GL_TEXTURE_CUBE_MAP_NEGATIVE_Z,
-    GL_TEXTURE_CUBE_MAP_POSITIVE_X,
-    GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
-    GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
-  };
   int i;
 
   if( !_rkglTextureBumpNormalMap( bump, filename ) ) return false;
@@ -338,7 +279,7 @@ bool rkglTextureBumpReadFile(zTexture *bump, char *filename)
   glBindTexture( GL_TEXTURE_2D, bump->id_bump );
   _rkglTextureBumpLightMap( bump );
   for( i=0; i<6; i++ )
-    glTexImage2D( cmap_type[i], 0, GL_RGBA, bump->width/2, bump->height/2, 0, GL_RGBA, GL_UNSIGNED_BYTE, bump->lbuf[i] );
+    glTexImage2D( rkgl_cubemap_id[i], 0, GL_RGBA, bump->width/2, bump->height/2, 0, GL_RGBA, GL_UNSIGNED_BYTE, bump->lbuf[i] );
 
   glTexParameteri( GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
   glTexParameteri( GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
@@ -348,7 +289,6 @@ bool rkglTextureBumpReadFile(zTexture *bump, char *filename)
   glTexEnvi( GL_TEXTURE_ENV, GL_COMBINE_RGB, GL_DOT3_RGBA );
   glTexEnvi( GL_TEXTURE_ENV, GL_SOURCE0_RGB, GL_TEXTURE0 );
   glTexEnvi( GL_TEXTURE_ENV, GL_SOURCE1_RGB, GL_TEXTURE1 );
-
   rkglTextureUnbind();
   return true;
 }
