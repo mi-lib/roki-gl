73647839409efa59e39ad136ea55f72c34a4c7ab
kdaic <develop@kdaic.net>
2024-01-11 19:26:56 +0900

    Simplified the roki_glfw to use global variables of rkgl_input

--- include/roki_gl/roki_glfw.h
+++ include/roki_gl/roki_glfw.h
@@ -16,14 +16,13 @@ __ROKI_GL_EXPORT int rkglInitGLFW(int *argc, char **argv);
 __ROKI_GL_EXPORT int rkglWindowCreateGLFW(GLFWwindow* window, int x, int y, int w, int h, const char *title);
 
 /* default callback functions */
-
 __ROKI_GL_EXPORT void rkglReshapeFuncGLFW(GLFWwindow* window, int w, int h);
 __ROKI_GL_EXPORT void rkglCharFuncGLFW(GLFWwindow* window, unsigned int codepoint);
 __ROKI_GL_EXPORT void rkglKeyFuncGLFW(GLFWwindow *window, int key, int scancode, int action, int mods);
 __ROKI_GL_EXPORT void rkglSpecialFuncGLFW(GLFWwindow* window, int key, int x, int y);
-__ROKI_GL_EXPORT void rkglMouseFuncGLFW(GLFWwindow* window, int button, int state, int x, int y);
+__ROKI_GL_EXPORT void rkglMouseFuncGLFW(GLFWwindow* window, int button, int state, int mods);
 __ROKI_GL_EXPORT void rkglMouseWheelFuncGLFW(GLFWwindow* window, double xoffset, double yoffset);
-__ROKI_GL_EXPORT void rkglMouseDragFuncGLFW(GLFWwindow* window, int x, int y);
+__ROKI_GL_EXPORT void rkglMouseDragFuncGLFW(GLFWwindow* window, double x, double y);
 __ROKI_GL_EXPORT void rkglVisFuncGLFW(GLFWwindow* window);
 
 __END_DECLS
