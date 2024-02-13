c728e9977b0444cb2304d489d90fb286b0893adb
kdaic <develop@kdaic.net>
2024-02-13 09:21:02 +0900

    Delete unnecessary changes

--- example/chain/pin_drag_sample.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export PATH=/home/daishi/work/program/robot_systems/milib/milib-starter/bin:$PATH
-export LD_LIBRARY_PATH=/home/daishi/work/program/robot_systems/milib/milib-starter/lib:$LD_LIBRARY_PATH
-
-/home/daishi/work/program/robot_systems/milib/milib-starter/roki-gl/example/chain/pin_drag_sample /home/daishi/work/program/robot_systems/milib/milib-starter/roki-gl/example/model/mighty.ztk
